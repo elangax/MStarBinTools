cca7cf7afb955d38e80d0f8ab93adbd6d76be812
nop404 <nop404@qq.com>
2018-06-08 19:22:39 +0800

    内置7zip解压，自动更新配置文件日期
    7zip touch工具仅适用于windows 64位平台。 touch.exe由cygwin中提取。

--- De_Android/android/cuscmd.bat
+++ De_Android/android/cuscmd.bat
@@ -1,14 +1,13 @@
-
 rd /s/q tmp_tvcustomer
-7z X -y tvcustomer -otmp_customer
+.\7-Zip\7z.exe X -y tvcustomer -otmp_customer
 echo "tvcustomer Decompress Successful"
 
 rd /s/q tmp_tvconfig
-7z X -y tvconfig -otmp_tvconfig
+.\7-Zip\7z.exe X -y tvconfig -otmp_tvconfig
 echo "tvconfig Decompress Successful"
 
 rd /s/q tmp_system
-7z X -y system -otmp_system
+.\7-Zip\7z.exe X -y system -otmp_system
 echo "system Decompress Successful"
 rd /s/q config
 mkdir config
@@ -21,6 +20,18 @@ xcopy tmp_tvconfig\boot3.jpg                                 .\config\
 xcopy tmp_tvconfig\config\model\Customer_1.ini               .\config\
 xcopy tmp_tvconfig\config\panel\FullHD_CMO216_H1L01.ini      .\config\
 xcopy tmp_customer\Customer\user_setting.sql                 .\config\
+xcopy tmp_customer\Customer\ZVT\                             .\config\ /s   
+
+touch .\config\build.prop
+touch .\config\customized_data.tar.gz
+touch .\config\boot0.jpg 
+touch .\config\boot1.jpg 
+touch .\config\boot2.jpg 
+touch .\config\boot3.jpg  
+touch .\config\Customer_1.ini
+touch .\config\FullHD_CMO216_H1L01.ini
+touch .\config\user_setting.sql
+
 del 0x*
 del system
 del RTPM
@@ -31,6 +42,7 @@ del userdata
 del lzo_temp
 del vsdcard
 del boot
+del tvconfig
 del tvcustomer
 del cache
 del ZVT_FACTORY_AT.txt
